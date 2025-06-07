77a95eaddafe98ab780f16416ff29beb037e71fb
Heiko Liebscher <hliebscher@idn.de>
2025-06-07 20:15:36 +0200

    fix

--- alde_components/truma_inetbox/climate/__init__.py
+++ alde_components/truma_inetbox/climate/__init__.py
@@ -28,13 +28,14 @@ def set_default_based_on_type():
     return set_defaults_
 
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+CONFIG_SCHEMA = climate.climate_schema(TrumaClimate).extend(
     {
         cv.GenerateID(): cv.declare_id(TrumaClimate),
         cv.GenerateID(CONF_TRUMA_INETBOX_ID): cv.use_id(TrumaINetBoxApp),
         cv.Required(CONF_TYPE): cv.enum(CONF_SUPPORTED_TYPE, upper=True),
     }
 ).extend(cv.COMPONENT_SCHEMA)
+
 FINAL_VALIDATE_SCHEMA = set_default_based_on_type()
 
 
--- components/truma_inetbox/climate/__init__.py
+++ components/truma_inetbox/climate/__init__.py
@@ -28,14 +28,16 @@ def set_default_based_on_type():
     return set_defaults_
 
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+CONFIG_SCHEMA = number.number_schema(TrumaNumber).extend(
     {
-        cv.GenerateID(): cv.declare_id(TrumaClimate),
+        cv.GenerateID(): cv.declare_id(TrumaNumber),
         cv.GenerateID(CONF_TRUMA_INETBOX_ID): cv.use_id(TrumaINetBoxApp),
         cv.Required(CONF_TYPE): cv.enum(CONF_SUPPORTED_TYPE, upper=True),
+        cv.Optional(CONF_MAX_VALUE): cv.float_,
+        cv.Optional(CONF_MIN_VALUE): cv.float_,
+        cv.Optional(CONF_STEP): cv.positive_float,
     }
 ).extend(cv.COMPONENT_SCHEMA)
-FINAL_VALIDATE_SCHEMA = set_default_based_on_type()
 
 
 async def to_code(config):
